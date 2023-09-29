a2e2263a6a021953bac9196ca87a9fcbc19db723
Keganwealths <kevin.olome.16@gmail.com>
2023-09-29 08:41:32 +0300

    streamlit tutorials

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import os
+import numpy as np
+
+dataframe=pd.DataFrame(
+    np.random.randn(10,20),
+    columns=('col %d' %i for i in range(20)))
+st.dataframe(dataframe.style.highlight_max(axis=0))
